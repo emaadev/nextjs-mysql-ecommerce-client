a813802fb14ecf90b6c33c32f3696a1c15cc1c10
EmanuelNu <emanuel.nu02@gmail.com>
2024-01-30 17:21:33 -0300

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = ({ nameStore }: { nameStore: string }) => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2024 '{nameStore}'. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
