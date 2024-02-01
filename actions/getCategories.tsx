e7c393e3479323be550186ed0278b8446963f988
EmanuelNu <emanuel.nu02@gmail.com>
2024-02-01 14:03:59 -0300

    fixing a typescript error with the type of MainNav

--- actions/getCategories.tsx
+++ actions/getCategories.tsx
@@ -2,7 +2,7 @@ import { Category } from "@/interfaces";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-const getCategories = async (): Promise<Category> => {
+const getCategories = async (): Promise<Category[]> => {
   const res = await fetch(URL, { cache: "no-store" });
 
   return res.json();
